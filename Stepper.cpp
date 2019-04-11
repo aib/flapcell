574ed4a8ccc7204b72a15dabc6f068688ae1fa4d
aib <aibok42@gmail.com>
2019-04-12 00:45:58 +0300

    Add step count to constructor; add spin, stop, step and goTo motion commands

--- Stepper.cpp
+++ Stepper.cpp
@@ -6,25 +6,33 @@
 class Stepper {
 	int stepPin;
 	int homePin;
-
+	unsigned int stepsPerRevolution;
 	unsigned long halfStepTime;
+
+	int targetPosition;
+
 	int stepPinState;
 	unsigned long nextStepTime;
+	bool wasHome;
+	int stepsFromHome;
 
 public:
-	Stepper(int stepPin, int homePin, unsigned long stepTime = 1000ul)
-		:stepPin(stepPin), homePin(homePin)
+	Stepper(int stepPin, int homePin, unsigned int stepsPerRevolution, unsigned long stepTime = 1000ul)
+		:stepPin(stepPin), homePin(homePin), stepsPerRevolution(stepsPerRevolution)
 	{
 		halfStepTime = stepTime >> 1;
+		targetPosition = 0;
+
 		stepPinState = LOW;
 		nextStepTime = micros();
+		wasHome = true;
+		stepsFromHome = -1;
 	}
 
 	void setup()
 	{
 		pinMode(stepPin, OUTPUT);
 		pinMode(homePin, INPUT);
-
 		digitalWrite(stepPin, stepPinState);
 	}
 
@@ -32,10 +40,69 @@ public:
 	{
 		unsigned long now = micros();
 
-		if (UL_LESS(nextStepTime, now)) {
-			stepPinState = stepPinState == HIGH ? LOW : HIGH;
-			digitalWrite(stepPin, stepPinState);
+		bool doStep = (targetPosition == -1) || (targetPosition != currentPosition());
+		if (doStep && UL_LESS(nextStepTime, now)) {
+			step();
 			nextStepTime = now + halfStepTime;
+			checkHome();
 		}
+	}
+
+	void stop()
+	{
+		targetPosition = currentPosition();
+	}
+
+	void spin()
+	{
+		targetPosition = -1;
+	}
+
+	void step(int num)
+	{
+		targetPosition = normalizePosition(currentPosition() + num);
+	}
+
+	void goTo(int position)
+	{
+		targetPosition = normalizePosition(position);
+	}
+
+	void home()
+	{
+		goTo(0);
+	}
+
+	int currentPosition() { return stepsFromHome; }
+
+	bool isHome() { return stepsFromHome == 0; }
+
+private:
+	void step()
+	{
+		if (stepPinState == LOW) {
+			stepPinState = HIGH;
+			if (stepsFromHome != -1) {
+				stepsFromHome++;
+			}
+		} else {
+			stepPinState = LOW;
+		}
+		digitalWrite(stepPin, stepPinState);
+	}
+
+	void checkHome()
+	{
+		bool home = (digitalRead(homePin) == HIGH);
+		if (!wasHome && home) {
+			stepsFromHome = 0;
+		}
+		wasHome = home;
+	}
+
+	int normalizePosition(int position)
+	{
+		while (position < 0) position += stepsPerRevolution;
+		return position % stepsPerRevolution;
 	}
 };
