1ed0335537d119da528c4a528ee64e92a54eb21b
aib <aibok42@gmail.com>
2022-04-17 17:26:13 +0300

    Add 'home' and 'print position' commands

--- FlapDriver.h
+++ FlapDriver.h
@@ -17,7 +17,32 @@ public:
 
 	void setup()
 	{
+		addCommand('H', home, "Home all cells");
+		addCommand('p', printpos, "Print cell positions");
+
 		StreamDriver::setup();
+	}
+
+private:
+	static void home(FlapDriver* driver, const char* cmd, size_t cmdLen)
+	{
+		for (size_t i = 0; i < driver->stepperCount; i++) {
+			driver->steppers[i].home();
+		}
+	}
+
+	static void printpos(FlapDriver* driver, const char* cmd, size_t cmdLen)
+	{
+		driver->stream.print("p");
+		for (size_t i = 0; i < driver->stepperCount; i++) {
+			driver->stream.print(" ");
+
+			driver->stream.print(driver->steppers[i].currentPosition());
+			if (driver->steppers[i].isHome()) {
+				driver->stream.print("H");
+			}
+		}
+		driver->stream.println();
 	}
 };
 
